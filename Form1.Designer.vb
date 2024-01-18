ed06f359eb9201af5b9b5ce1d23227ca98a1642f
Lenovo-User <Lenovo-User@DESKTOP-7L4SHKG>
2024-01-18 16:43:35 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Button1 = New Button()
+        ComboBox1 = New ComboBox()
+        TextBox1 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(160, 62)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(179, 25)
+        Label1.TabIndex = 0
+        Label1.Text = "Enter the Grade letter"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(276, 135)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(67, 25)
+        Label2.TabIndex = 0
+        Label2.Text = "Results"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(316, 232)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 1
+        Button1.Text = "Compute!"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.BackColor = Color.White
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(425, 61)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(182, 33)
+        ComboBox1.TabIndex = 2
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(427, 136)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(331, 31)
+        TextBox1.TabIndex = 3
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(0), CByte(192), CByte(0))
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox1)
+        Controls.Add(ComboBox1)
+        Controls.Add(Button1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox1 As TextBox
+
+End Class
